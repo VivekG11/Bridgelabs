39bb292a384176c35cbaa14084737fd9a67632fd
VivekG11 <vivekvk2903@gmail.com>
2021-06-25 18:09:09 +0530

    Brdige labs

---
+++ BridgeLabz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BridgeLabz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 11;
+            int b = 29;
+            Console.WriteLine("The sum is :"+(a+b));
+            Console.WriteLine(".Net framework");
+            Console.WriteLine("Hello World!");
+            add();
+          
+        }
+        public static void add()
+        {
+            int x = 6;
+            int y = 5;
+            int sum = x + y;
+            Console.WriteLine(sum);
+        }
+    }
+}
